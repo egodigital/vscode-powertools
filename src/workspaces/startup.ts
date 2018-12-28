2a9e4d8a4549107cd80d210f860eada147e61c9e
Marcel Kloubert <marcel.kloubert@gmx.net>
2018-12-28 03:02:31 +0100

    improved running shell commands

--- src/workspaces/startup.ts
+++ src/workspaces/startup.ts
@@ -21,6 +21,7 @@ import * as ego_contracts from '../contracts';
 import * as ego_helpers from '../helpers';
 import * as ego_workspace from '../workspace';
 import * as path from 'path';
+import * as vscode from 'vscode';
 
 
 /**
@@ -72,41 +73,79 @@ export async function onStartup() {
                     {
                         const SHELL_ITEM = <ego_contracts.ShellCommandStartupItem>entry;
 
-                        const CMD = WORKSPACE.replaceValues(SHELL_ITEM.command);
-                        if ('' === CMD.trim()) {
-                            continue;
-                        }
+                        const COMMAND_TO_EXECUTE = WORKSPACE.replaceValues(
+                            SHELL_ITEM.command
+                        );
 
-                        let currentWorkDirectory = WORKSPACE.replaceValues(SHELL_ITEM.cwd);
-                        if ('' === currentWorkDirectory.trim()) {
-                            currentWorkDirectory = WORKSPACE.rootPath;
+                        let cwd = WORKSPACE.replaceValues(
+                            SHELL_ITEM.cwd
+                        );
+                        if (ego_helpers.isEmptyString(cwd)) {
+                            cwd = WORKSPACE.rootPath;
                         }
-                        if (!path.isAbsolute(currentWorkDirectory)) {
-                            currentWorkDirectory = path.join(
-                                WORKSPACE.rootPath, currentWorkDirectory
+                        if (!path.isAbsolute(cwd)) {
+                            cwd = path.join(
+                                WORKSPACE.rootPath, cwd
                             );
                         }
+                        cwd = path.resolve(cwd);
 
                         const SILENT = ego_helpers.toBooleanSafe(SHELL_ITEM.silent, true);
 
-                        try {
-                            WORKSPACE.logger
-                                    .info(`Executing '${ CMD }' in shell ...`);
+                        // run command
+                        await vscode.window.withProgress({
+                            cancellable: false,
+                            location: vscode.ProgressLocation.Notification,
+                            title: 'Shell Command',
+                        }, (progress) => {
+                            return new Promise<void>((resolve, reject) => {
+                                const COMPLETED = (err: any, result?: string) => {
+                                    const WRITE_RESULT = () => {
+                                        if (!SILENT) {
+                                            if (!ego_helpers.isEmptyString(result)) {
+                                                WORKSPACE.output
+                                                    .appendLine(ego_helpers.toStringSafe(result));
+                                                WORKSPACE.output
+                                                    .appendLine('');
+                                            }
+                                        }
+                                    };
 
-                            const RESULT = childProcess.execSync(CMD, {
-                                cwd: path.resolve(
-                                    currentWorkDirectory
-                                ),
+                                    if (err) {
+                                        WORKSPACE.output
+                                            .appendLine(`[FAILED: '${ ego_helpers.errorToString(err) }']`);
+
+                                        WRITE_RESULT();
+
+                                        reject(err);
+                                    } else {
+                                        WORKSPACE.output
+                                            .appendLine('[OK]');
+
+                                        WRITE_RESULT();
+
+                                        resolve();
+                                    }
+                                };
+
+                                try {
+                                    WORKSPACE.output
+                                        .append(`Running shell command '${ COMMAND_TO_EXECUTE }' ... `);
+
+                                    progress.report({
+                                        message: `Running '${ COMMAND_TO_EXECUTE }' ...`,
+                                    });
+
+                                    childProcess.exec(COMMAND_TO_EXECUTE, {
+                                        cwd: cwd,
+                                    }, (err, result) => {
+                                        COMPLETED(err, result);
+                                    });
+                                } catch (e) {
+                                    COMPLETED(e);
+                                }
                             });
-
-                            if (!SILENT) {
-                                WORKSPACE.output
-                                        .appendLine(RESULT.toString('utf8'));
-                            }
-                        } catch (e) {
-                            WORKSPACE.logger
-                                    .err(e, 'startups.onStartup(2)');
-                        }
+                        });
                     }
                     break;
 
